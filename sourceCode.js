265c679884781b80783c56542a6391f2fdbcdf99
quangtruonglf <quangtruonglf@gmail.com>
2020-06-29 17:48:29 +0700

    signin\2

--- sourceCode.js
+++ sourceCode.js
@@ -1,1 +1,25 @@
-//Sign-in Page
+//signIn.html
+let allUserAcc = [];
+
+function saveUserAcc() {
+}
+
+function directCreateAcc() {
+    window.location.href = "createAcc.html";
+}
+
+//createAcc.html
+function createAcc() {
+    let oneUserAcc = {};
+    oneUserAcc["firstName"] = document.getElementById("firstName").value;
+    oneUserAcc["lastName"] = document.getElementById("lastName").value;
+    oneUserAcc["email"] = document.getElementById("email_createAcc").value;
+    oneUserAcc["password"] = document.getElementById("password_createAcc").value;
+    allUserAcc.push(oneUserAcc);
+    console.log(allUserAcc);
+    window.location.href = "signIn.html";
+}
+
+localStorage["allUserAcc"] = JSON.stringify(allUserAcc);
+allUserAcc = JSON.parse(localStorage.allUserAcc)
+console.log(allUserAcc)
